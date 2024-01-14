e619def181b1da548b30d92629bbecba76a4b065
sumoduduk <sumoduduk13@gmail.com>
2024-01-14 19:21:00 +0700

    why mdx not work

--- lua/calista/plugins/treesitter.lua
+++ lua/calista/plugins/treesitter.lua
@@ -1,23 +1,29 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"javascript",
-				"html",
-				"typescript",
-				"tsx",
-				"rust",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "javascript",
+        "html",
+        "typescript",
+        "tsx",
+        "rust",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "dockerfile",
+        "graphql",
+        "toml",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
