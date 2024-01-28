07c435fb9f57882ea0ff659ef21089ef7b2b6480
sumoduduk <sumoduduk@example.com>
2024-01-28 18:36:35 +0700

    feat: add lazygit inside neovim
    - add lazygit while editing
    - add keybinding to enter lazygit

--- lua/calista/plugins/terminal.lua
+++ lua/calista/plugins/terminal.lua
@@ -12,7 +12,7 @@ return {
     local term_map = require("terminal.mappings")
     -- vim.keymap.set({ "n", "x" }, "<leader>ts", term_map.operator_send, { expr = true })
     -- vim.keymap.set("n", "<leader>to", term_map.toggle)
-    vim.keymap.set("n", "<leader>tm", term_map.toggle({ open_cmd = "enew" }))
+    -- vim.keymap.set("n", "<leader>tm", term_map.toggle({ open_cmd = "enew" }))
     -- vim.keymap.set("n", "<leader>tr", term_map.run)
     -- vim.keymap.set("n", "<leader>tR", term_map.run(nil, { layout = { open_cmd = "enew" } }))
     -- vim.keymap.set("n", "<leader>tk", term_map.kill)
@@ -32,6 +32,18 @@ return {
     vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]])
     vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
 
-    vim.env["GIT_EDITOR"] = "nvr -cc close -cc tabnew --remote-wait +'set bufhidden=wipe'"
+    local lazygit = require("terminal").terminal:new({
+      layout = { open_cmd = "float", height = 0.9, width = 0.9 },
+      cmd = { "lazygit" },
+      autoclose = true,
+    })
+
+    vim.env["GIT_EDITOR"] = "nvr -cc close -cc split --remote-wait +'set bufhidden=wipe'"
+    vim.api.nvim_create_user_command("Lazygit", function(args)
+      lazygit.cwd = args.args and vim.fn.expand(args.args)
+      lazygit:toggle(nil, true)
+    end, { nargs = "?" })
+
+    vim.keymap.set("n", "<leader>lg", ":Lazygit<CR>")
   end,
 }
