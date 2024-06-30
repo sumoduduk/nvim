e1afb74fbeee746951f231871ab9502ff6dfcf40
sumoduduk <sumoduduk13@gmail.com>
2024-06-30 13:19:41 +0700

    fix neotree

--- lua/calista/plugins/neo-tree.lua
+++ lua/calista/plugins/neo-tree.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -20,11 +19,42 @@ return {
       --   },
       -- },
       default_component_configs = {
-        icon = {
-          folder_closed = "",
-          folder_open = "",
-        },
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "󰉖",
+            folder_empty_open = "󰷏",
       },
+       git_status = {
+            symbols = {
+              -- Change type
+              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "✖",-- this can only be used in the git_status source
+              renamed   = "󰁕",-- this can only be used in the git_status source
+              -- Status type
+              untracked = "",
+              ignored   = "",
+              unstaged  = "󰄱",
+              staged    = "",
+              conflict  = "",
+            }
+      },
+           file_size = {
+            enabled = false,
+            required_width = 64, -- min width of window required to show this column
+          },
+          type = {
+            enabled = true,
+            required_width = 122, -- min width of window required to show this column
+          },
+          last_modified = {
+            enabled = false,
+            required_width = 88, -- min width of window required to show this column
+          },
+          created = {
+            enabled = false,
+            required_width = 110, -- min width of window required to show this column
+          },
       window = {
         width = 30,
         mappings = {
