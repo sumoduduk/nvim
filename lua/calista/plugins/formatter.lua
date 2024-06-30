3fd4530000ba9f0065df3b0b7120c7311d206b4c
sumoduduk <sumoduduk13@gmail.com>
2024-06-30 13:19:53 +0700

    feat: add alejandra

--- lua/calista/plugins/formatter.lua
+++ lua/calista/plugins/formatter.lua
@@ -21,6 +21,7 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
         rust = { "rust_analyzer" },
+        nix = { "alejandra" },
       },
       format_on_save = {
         lsp_fallback = true,
