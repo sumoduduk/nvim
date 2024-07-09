6c6ad317745930c9350db1d2aba3851b8db8acdc
sumoduduk <sumoduduk13@gmail.com>
2024-07-09 16:11:57 +0700

    update

--- lua/calista/plugins/lsp/lspconfig.lua
+++ lua/calista/plugins/lsp/lspconfig.lua
@@ -56,10 +56,14 @@ return {
       keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
       opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      vim.keymap.set('n', '[d', function() -- previous
+        vim.diagnostic.jump({ count = -vim.v.count1 })
+      end, opts)
 
       opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      vim.keymap.set('n', ']d', function() -- next
+        vim.diagnostic.jump({ count = vim.v.count1 })
+      end, opts)
 
       opts.desc = "Show documentation for what is under cursor"
       keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
