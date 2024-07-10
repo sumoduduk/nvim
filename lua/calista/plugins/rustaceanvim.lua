959eb04534bcaac3b93f2f04175f9252c164a526
sumoduduk <sumoduduk13@gmail.com>
2024-07-10 22:51:13 +0700

    add termgui colors = true

--- lua/calista/plugins/rustaceanvim.lua
+++ lua/calista/plugins/rustaceanvim.lua
@@ -1,7 +1,6 @@
 return {
   'mrcjkb/rustaceanvim',
   version = '^4', -- Recommended
-  ft  = 'rust',
   lazy = false, -- This plugin is already lazy
   config = function ()
   vim.g.rustaceanvim = {
