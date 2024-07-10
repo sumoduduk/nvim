f5a7abeb4b4fe5cbdd8d97f4f24e2c55ee0e78a3
sumoduduk <sumoduduk13@gmail.com>
2024-07-10 16:09:49 +0700

    add check command in rust analyzer

--- lua/calista/plugins/rustaceanvim.lua
+++ lua/calista/plugins/rustaceanvim.lua
@@ -1,5 +1,16 @@
 return {
   'mrcjkb/rustaceanvim',
   version = '^4', -- Recommended
+  ft  = 'rust',
   lazy = false, -- This plugin is already lazy
+  config = function ()
+  vim.g.rustaceanvim = {
+  -- LSP configuration
+  server = {
+				check = {
+					overrideCommand = { "cargo", "check", "--message-format=json" },
+				},
+  },
+}
+  end,
 }
