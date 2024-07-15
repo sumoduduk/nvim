aff43e8f01c4de79dfd063fa155c99a8767bce23
sumoduduk <sumoduduk13@gmail.com>
2024-07-15 15:10:55 +0700

    change keymap for multi cursor

--- lua/calista/plugins/vim-multi.lua
+++ lua/calista/plugins/vim-multi.lua
@@ -2,9 +2,9 @@ return {
   "mg979/vim-visual-multi",
   lazy = false,
   init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = '<leader>n',
-				["Find Subword Under"] = '<leader>n',
-			}
-		end,
+    vim.g.VM_maps = {
+      ["Find Under"] = "<leader>m",
+      ["Find Subword Under"] = "<leader>m",
+    }
+  end,
 }
