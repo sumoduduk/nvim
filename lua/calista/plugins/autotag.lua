7b63ad93b3d3cae4cdd3c3b9cb103cf986dee124
sumoduduk <sumoduduk13@gmail.com>
2024-08-21 10:36:18 +0700

    fix: auto tag close html

--- lua/calista/plugins/autotag.lua
+++ lua/calista/plugins/autotag.lua
@@ -1,12 +1,22 @@
 return {
-	"windwp/nvim-ts-autotag",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "windwp/nvim-ts-autotag",
+  config = function()
+    local configs = require("nvim-ts-autotag")
 
-		configs.setup({
-			autotag = {
-				enable = true,
-			},
-		})
-	end,
+    configs.setup({
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
+      -- Also override individual filetype configs, these take priority.
+      -- Empty by default, useful if one of the "opts" global settings
+      -- doesn't work well in a specific filetype
+      per_filetype = {
+        ["html"] = {
+          enable_close = false,
+        },
+      },
+    })
+  end,
 }
