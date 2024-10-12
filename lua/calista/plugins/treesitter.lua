5014f284b892bdac63ec83e1d7fa141d5ebd28fd
sumoduduk <sumoduduk13@gmail.com>
2024-10-12 12:40:42 +0700

    feat: addd tact treesitter

--- lua/calista/plugins/treesitter.lua
+++ lua/calista/plugins/treesitter.lua
@@ -30,5 +30,25 @@ return {
       indent = { enable = true },
     })
     vim.treesitter.language.register("markdown", { "mdx" })
+
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    -- Adds tree-sitter-tact support
+    parser_config.tact = {
+      install_info = {
+        url = "~/File/treesitter/tree-sitter-tact",
+        files = { "src/parser.c" },
+        branch = "main",
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+    }
+
+    -- Adds filetype recognition for .tact files
+    vim.filetype.add({
+      extension = {
+        tact = "tact",
+      },
+    })
   end,
 }
