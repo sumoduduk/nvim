7d19ee7b640d38c5987d5ab5337848b1c58c17cc
sumoduduk <sumoduduk13@gmail.com>
2024-11-21 05:09:20 +0700

    feat: add dockerfile and docker compose lsp

--- lua/calista/plugins/lsp/lspconfig.lua
+++ lua/calista/plugins/lsp/lspconfig.lua
@@ -88,6 +88,16 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["docker_compose_language_service"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
+    lspconfig["dockerls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     lspconfig["nil_ls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
